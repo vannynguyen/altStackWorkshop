1f52e2a31ecf58299261bd600033716115902ae2
Vanny Nguyen <van.p.nguyen.17@dartmouth.edu>
2017-05-04 15:06:19 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 // SCROLL DOWN FOR DIRECTIONS
 
-var user;
-document.addEventListener('DOMContentLoaded', function() {
- var loggedInUser = localStorage.getItem('username');
- if (loggedInUser != null ){
-   document.getElementById('hello').innerText = `Hi, ${loggedInUser}!`;
-   document.getElementById('hello').style.display = 'block';
-   user = loggedInUser;
- }
- else{
-   user = 'Anonymous';
- }
+let user;
+document.addEventListener('DOMContentLoaded', () => {
+  const loggedInUser = localStorage.getItem('username');
+  if (loggedInUser != null) {
+    document.getElementById('hello').innerText = `Hi, ${loggedInUser}!`;
+    document.getElementById('hello').style.display = 'block';
+    user = loggedInUser;
+  } else {
+    user = 'Anonymous';
+  }
 }, false);
 
-var nav = new Vue({
+const nav = new Vue({
   el: '#fake-nav',
   methods: {
-    open: function(which, e) {
+    open(which, e) {
       e.preventDefault();
       modal.active = which;
       console.log(modal.active);
     },
-  }
+  },
 });
 
-var modal_submit_login = 'Login';
+const modal_submit_login = 'Login';
 // var user = 'Anonymous';
 
 var modal = new Vue({
@@ -37,23 +36,62 @@ var modal = new Vue({
     loginUser: '',
   },
   methods: {
-    close: function(e) {
+    addEvent() {
+      if (this.event.name) {
+        this.event.user = user;
+        // push the event to this.events below!
+
+        this.event = { name: '', user: '', description: '', date: '' };
+      }
+    },
+    fetchEvents() {
+      const events = [];
+      this.events = events;
+    },
+    close(e) {
       e.preventDefault();
       if (e.target === this.$el) {
         this.active = null;
       }
     },
-    submit: function(which, e) {
-
+    deleteEvent(index) {
+      if (confirm('Are you sure you want to delete this event?')) {
+        // $remove is a Vue convenience method similar to splice
+        this.events.splice(index, 1);
+      }
+    },
+    submit(which, e) {
       user = document.getElementById('username').value;
       localStorage.setItem('username', user);
       document.getElementById('hello').innerText = `Hi, ${user}!`;
-      document.getElementById('hello').style.display = "block";
+      document.getElementById('hello').style.display = 'block';
       modal.active = null;
-
-
-    }
-  }
+    },
+  },
 });
 
 // ADD CODE HERE TO CREATE A VUE INSTANCE
+
+const eventsPage = new Vue({
+
+  // We want to target the div with an id of 'events'
+  el: '#events',
+
+  // Here we can register any values or collections that hold data
+  // for the application
+  data: {
+    event: { user: '', name: '', description: '', date: '' },
+    events: [],
+  },
+
+  // Anything within the mounted function will run when the application loads
+  mounted() {
+    // call fetchEvents here!
+    this.fetchEvents();
+  },
+
+  // Methods we want to use in our application are registered here
+  methods: {
+    // add fetchEvents, addEvent, and deleteEvent
+  },
+});
